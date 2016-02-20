febb367be9ad713d6d4b4baadd3a7c2bdfaf52c8
Kay Cha <kay@hannal.net>
2016-02-20 14:09:47 +0900

    add blog 앱과 모델

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+# blog/models.py
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    is_model_field = False
+
