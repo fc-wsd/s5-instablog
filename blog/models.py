4d5ceec54b15fe36d458588bbb57550a1efade9b
Kay Cha <kay@hannal.net>
2016-02-20 17:05:20 +0900

    fix trivial changes

--- blog/models.py
+++ blog/models.py
@@ -20,3 +20,5 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return '{} of {}'.format(self.pk, self.post.pk)
