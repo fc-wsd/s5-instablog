e37c4600b837bb85159b853248fb57bcd38fc13a
Kay Cha <kay@hannal.net>
2016-02-27 16:08:47 +0900

    add paginator

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,9 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+from django.core.paginator import PageNotAnInteger
+from django.core.paginator import EmptyPage
 
 from .models import Post
 
@@ -16,13 +19,24 @@ def hello_with_template(request):
 
 
 def list_posts(request):
+    per_page = 2
+    current_page = request.GET.get('page', 1)
+
     all_posts = Post.objects \
             .select_related() \
             .prefetch_related() \
             .all().order_by('-pk')
 
+    pagi = Paginator(all_posts, per_page)
+    try:
+        pg = pagi.page(current_page)
+    except PageNotAnInteger:
+        pg = pagi.page(1)
+    except EmptyPage:
+        pg = []
+
     return render(request, 'list_posts.html', {
-        'posts': all_posts,
+        'posts': pg,
     })
 
 
